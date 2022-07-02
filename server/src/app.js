45db52085406e44c9e224df15149e7efa6d965cc
kamilspyrka <kamilspyrka12@gmail.com>
2022-07-02 15:11:49 +0200

    sqlite

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
+//App
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) =>{
-    res.send({
-        message: `hi ${req.body.email}`
-    })
-})
+//Routes
+require('./routes')(app)
 
-app.listen(process.env.PORT || 5000)
+//DB
+sequelize.sync()
+.then(() =>{
+    app.listen(config.port)
+    console.log(`server started on port: ${config.port}`)
+})
---
+++ server/src/models/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const config = require('../config/config')
+const db = {}
+
+//Connect to DB
+const sequelize = new Sequelize(
+    config.db.database,
+    config.db.user,
+    config.db.password,
+    config.db.options,
+)
+
+//Make creating future models easier
+fs
+    .readdirSync(__dirname)
+    .filter(file => 
+        file !== 'index.js'     
+    )
+    .forEach((file) => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize)
+        db[model.name] = model
+    })
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
